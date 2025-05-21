841f349ee6c980f140f4c72012470af6790c7029
hugozera1 <hugorodrigues1052@gmail.com>
2025-05-21 20:57:53 -0300

    cors

--- api/divide.js
+++ api/divide.js
@@ -1,5 +1,6 @@
 export default function handler(req, res) {
   const { a, b } = req.query;
+   res.setHeader('Access-Control-Allow-Origin', '*');
   if (isNaN(a) || isNaN(b)) {
     return res.status(400).json({ error: 'Parâmetros inválidos' });
   }

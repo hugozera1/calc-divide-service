1b51373071082435c0ef0ca9b12eaf1e69a84c55
hugozera1 <hugorodrigues1052@gmail.com>
2025-05-21 21:11:56 -0300

    corsss

--- api/divide.js
+++ api/divide.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
   const { a, b } = req.query;
-   res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Access-Control-Allow-Origin', 'https://calc-frontend-woad.vercel.app/');
   if (isNaN(a) || isNaN(b)) {
     return res.status(400).json({ error: 'Parâmetros inválidos' });
   }
